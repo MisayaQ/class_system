62f146f88030a379762bcf513e18dba3ae0585d8
david <zhangdawei@91isoft.com>
2020-02-01 19:38:01 +0800

    统计购买总数

--- src/main/java/com/course/controller/CoursePurchaseController.java
+++ src/main/java/com/course/controller/CoursePurchaseController.java
@@ -117,7 +117,46 @@ public class CoursePurchaseController {
         }
     }
 
-    @ApiOperation(value="统计", notes="")
+    @ApiOperation(value="统计总数", notes="")
+    @GetMapping("/getPurchaseByCountAll")
+    public Ret getPurchaseByCountAll(String countType) throws Exception{
+        QueryWrapper queryWrapper = new QueryWrapper();
+        Date stDate = new Date();
+        Date enDate = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String endStr = sdf.format(enDate);
+        Calendar cal = Calendar.getInstance();
+        List<CountResult> getList = new ArrayList<>();
+        CoursePurchase coursePurchase = new CoursePurchase();
+        if ("day".equals(countType)) {
+            cal.add(Calendar.DATE,-7);
+            stDate = cal.getTime();
+            String startStr = sdf.format(stDate);
+        }else if ("month".equals(countType)) {
+            int month = cal.get(Calendar.MONTH) + 1;
+            int year = cal.get(Calendar.YEAR);
+            String date = String.valueOf(year) + "-" + String.valueOf(month) + "-01 00:00:00";
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            stDate = format.parse(date);
+        } else if ("year".equals(countType)) {
+            int year = cal.get(Calendar.YEAR);
+            String date = String.valueOf(year) + "-01-01 00:00:00";
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            stDate = format.parse(date);
+        } else {
+            return Ret.error().setMsg("只能为day,month,year类型");
+        }
+        queryWrapper.gt("created_time",stDate);
+        queryWrapper.lt("created_time",enDate);
+        getList = iCoursePurchaseService.list(queryWrapper);
+        int counts = 0;
+        if (getList != null && !getList.isEmpty()) {
+            counts = getList.size();
+        }
+        return Ret.ok().setData(counts);
+    }
+
+    @ApiOperation(value="统计图", notes="")
     @GetMapping("/getPurchaseByCount")
     public Ret getPurchaseByCount(String countType) throws Exception{
         QueryWrapper queryWrapper = new QueryWrapper();
